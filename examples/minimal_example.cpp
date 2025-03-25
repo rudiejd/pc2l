7ab05a68922724e76ba95a7c8f3321573391d60e
JD Rudie <jd@rudiejd.io>
2025-03-25 08:28:15 -0400

    add minimal example

---
+++ examples/minimal_example.cpp
@@ -0,0 +1,32 @@
+#ifndef MINIMAL_EXAMPLE_CPP
+#define MINIMAL_EXAMPLE_CPP
+
+#include "MPIHelper.h"
+#include <pc2l.h>
+
+int main(int argc, char *argv[]) {
+  // Boilerplate code to get MPI up and running
+  auto &pc2l = pc2l::System::get();
+  pc2l.initialize(argc, argv);
+  // 50 block cache
+  pc2l.setCacheSize(50 * 100 * sizeof(int));
+  pc2l.start();
+
+  if (pc2l::MPI_GET_RANK() == 0) {
+    
+    pc2l::Vector<int, 8 * sizeof(int)> vec(100);
+
+    for (auto i = 0; i < 100; i++) {
+      vec.push_back(i);
+    }
+
+    for (auto i = 0; i < vec.size(); i++) {
+      std::cout << "vec[" << i << "]=" << vec[i] << std::endl;
+    }
+  }
+
+  pc2l.stop();
+  pc2l.finalize();
+}
+
+#endif
